35e7659b41ee529c82a8bf51db67bc83fa37ef0d
Boby <liya@sliya.in>
2025-02-02 14:23:39 +0530

    only window

---
+++ src/log_viewer.rs
@@ -0,0 +1,19 @@
+use eframe::{App, Frame};
+use egui::Context;
+
+pub struct LogViewer {
+
+}
+
+impl LogViewer {
+    pub fn new() -> Self {
+        Self {
+
+        }
+    }
+}
+
+impl App for LogViewer {
+    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+mod log_viewer;
+
+use egui::ViewportBuilder;
+use crate::log_viewer::LogViewer;
+
+const APP_NAME: &'static str = "LogViewer";
+
+
+fn main() -> eframe::Result {
+
+    let native_options = eframe::NativeOptions {
+        viewport: ViewportBuilder::default()
+            .with_inner_size([1280., 720.]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+       APP_NAME,
+       native_options,
+       Box::new(|cc| {
+           Ok(Box::new(LogViewer::new()))
+       })
+    )
+}
+
