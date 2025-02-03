df2e5506a5587546abed5e9d454e3e52a5138db4
Boby <liya@sliya.in>
2025-02-03 07:38:33 +0530

    improve performance

--- src/log_viewer.rs
+++ src/log_viewer.rs
@@ -1,11 +1,12 @@
 use crate::indexer::Indexer;
 use crate::string_ext::StringExt;
 use eframe::{App, Frame};
-use egui::{Context, Stroke, Ui};
-use egui::style::TextCursorStyle;
+use egui::{Context, Ui};
 use egui_extras::{Column, TableBuilder};
+use tokio::time::Instant;
 
 pub struct LogViewer {
+    t: Instant,
     search_query: String,
     status_bar_infos: Vec<String>,
     results: Vec<String>,
@@ -15,6 +16,7 @@ impl LogViewer {
     pub fn new() -> Self {
         println!("no");
         Self {
+            t: Instant::now(),
             search_query: "".to_string(),
             status_bar_infos: vec![String::from("indexing"), String::from("searching")],
             results: vec![],
@@ -22,16 +24,19 @@ impl LogViewer {
     }
 
     fn status_bar_ui(&mut self, ctx: &Context, ui: &mut Ui) {
-        for info in &self.status_bar_infos {
-            ui.horizontal(|ui| {
+        
+        ui.horizontal(|ui| {
+            ui.label(format!("fps: {}", 1000 / self.t.elapsed().as_millis()));
+            self.t = Instant::now();
+            for info in &self.status_bar_infos {
                 let label_response = ui.label(info);
                 ui.add(
                     egui::widgets::Spinner::new()
                         .size(label_response.intrinsic_size.unwrap().y)
                         .color("#a1a1a1".hex_color()),
                 );
-            });
-        }
+            }
+        });
     }
 
     fn search_widget_ui(&mut self, ui: &mut Ui) {
@@ -43,55 +48,58 @@ impl LogViewer {
             ui.add(search_widget);
             if ui.button("search").clicked() {
                 let mut indexer = Indexer::new();
-                self.results =  indexer.query(self.search_query.clone()).collect::<Vec<String>>();
-                
+                self.results = indexer
+                    .query(self.search_query.clone())
+                    .collect::<Vec<String>>();
             }
 
             if ui.button("Index").clicked() {
                 let mut indexer = Indexer::new();
                 indexer.index_logfile();
-
             }
         });
     }
 
     fn search_results_ui(ui: &mut Ui, results: &Vec<String>) {
         // search results
-        // egui::ScrollArea::both().show(ui, |ui| {
-            let mut table = TableBuilder::new(ui)
-                .striped(true)
-                .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-                .column(Column::auto())
-                .column(
-                    Column::remainder()
-                        .at_least(40.0)
-                        .clip(false)
-                        .resizable(true),
-                )
-                .min_scrolled_height(0.0);
+        let mut table = TableBuilder::new(ui)
+            .striped(true)
+            .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+            .column(Column::auto())
+            .column(
+                Column::remainder()
+                    .at_least(40.0)
+                    .clip(false)
+                    .resizable(true),
+            )
+            .min_scrolled_height(0.0);
 
-            table
-                .header(20.0, |mut header| {
-                    header.col(|ui| {
-                        ui.strong("Timestamp");
-                    });
-                    header.col(|ui| {
-                        ui.strong("Search Results");
-                    });
-                })
-                .body(|mut body| {
-                    for i in results {
-                        body.row(20., |mut row| {
-                            row.col(|ui| {
-                                ui.label("00:00:00");
-                            });
-                            row.col(|ui| {
-                                ui.label(i);
-                            });
-                        });
-                    }
+        table
+            .header(20.0, |mut header| {
+                header.col(|ui| {
+                    ui.strong("Timestamp");
                 });
-        // });
+                header.col(|ui| {
+                    ui.strong("Search Results");
+                });
+            })
+            .body(|mut body| {
+                    
+                    
+                   body.rows(20., results.len(), |mut row| {
+                       let row_index = row.index();
+                       let val = &results[row_index];
+                       row.col(|ui| {
+                           ui.label("00:00:00");
+                       });
+                       row.col(|ui| {
+                           ui.label(val);
+                       });
+                       
+                   });
+                    
+            
+            });
     }
 }
 
